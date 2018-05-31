2e7fc927e415d1d28bf9663495731faf2f5d9d86
RicardoTAsefa <39797561+RicardoTAsefa@users.noreply.github.com>
2018-05-31 13:52:02 +0200

    Prueba

---
+++ pruebaGid/src/main/java/com/sunion/java/pruebaGid/App.java
@@ -0,0 +1,13 @@
+package com.sunion.java.pruebaGid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
